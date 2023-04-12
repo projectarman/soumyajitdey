0449c90051bd5fd7b1bf8031c9e1a36fd36f70ea
Soumyajit Dey <129984512+projectarman@users.noreply.github.com>
2023-04-12 14:52:26 +0530

    Update about-data.js

--- src/data/about-data.js
+++ src/data/about-data.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is ABU SAID. I'm a web designer and developer based in Dhaka, Bangladesh.",
-    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.",
+    description1: "My name is Soumyajit Dey. I'm a Student of Electronics  and communication Engineering based in : Baranagar,Kolkata,West Bengal,700036.",
+    description2: "I wish to pursue my career at a company that allows me to make the best use of my technical and interpersonal skills. I am willing to explore a wide range of opportunities that allow me to gain more knowledge and perspective .To work in an organization where I can use my skills and knowledge to deliver value-added results that provide me job satisfaction and self-development which help me achieve personal as well as organizational goals.",
     image: 1
 }
